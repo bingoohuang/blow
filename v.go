f760760636249c14f04bcf21222e79480cb0012c
bingoohuang <bingoo.huang@gmail.com>
2021-08-30 13:54:15 +0800

    v.go print go version more compactly

--- v.go
+++ v.go
@@ -12,8 +12,8 @@ var (
 )
 
 func Version() string {
-	return fmt.Sprintf("Version:\t%s\n", appVersion) +
-		fmt.Sprintf("Build:\t\t%s\n", buildTime) +
-		fmt.Sprintf("Git:\t\t%s\n", gitCommit) +
-		fmt.Sprintf("Go:\t\t%s\n", goVersion)
+	return fmt.Sprintf("version: %s\n", appVersion) +
+		fmt.Sprintf("build:\t%s\n", buildTime) +
+		fmt.Sprintf("git:\t%s\n", gitCommit) +
+		fmt.Sprintf("go:\t%s\n", goVersion)
 }
